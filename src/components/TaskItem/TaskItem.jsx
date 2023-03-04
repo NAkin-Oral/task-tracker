556b98661ce361c3e4b6b0ee8c1b9df69a506697
NAkin-Oral <nurettinakinoral@gmail.com>
2023-03-04 17:31:04 +0300

    First

---
+++ src/components/TaskItem/TaskItem.jsx
@@ -0,0 +1,30 @@
+import { Checkbox, ListItem, ListItemText, IconButton, Tooltip } from '@mui/material';
+import { MdCheckCircle, MdOutlineCircle, MdDelete } from "react-icons/md";
+import './TaskItem.scss';
+
+const TaskItem = ({ task, onComplete, onDelete}) => {
+
+  let { title, isCompleted, date, id } = task;
+  date = date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+  return (
+
+    <>
+      <ListItem disablePadding className={`TaskItem ${isCompleted&&'completed'}` }>
+        <Checkbox 
+          onChange={()=> onComplete(id)}
+          icon={ <MdOutlineCircle className='checkIcon' /> }
+          checkedIcon={ <MdCheckCircle className="checkIcon" /> }
+        />
+        <ListItemText primary={ title } secondary={ date} />
+        
+        <Tooltip title="Remove" arrow>
+          <IconButton onClick={()=> onDelete(id) }>
+            <MdDelete className='deleteIcon'/>
+          </IconButton>
+        </Tooltip>
+      </ListItem>
+    </>
+  )
+}
+
+export default TaskItem
